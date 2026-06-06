8b38f2e435d7b372e8c4a3efa5326427bdd44283
Igor Vish <igorv0@yahoo.com>
2026-06-06 06:41:40 -0500

    fix header issues

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,10 +6,10 @@ const Header = () => {
     return (
         <header className='header'>
             <div className='header-content'>
-                <img src={logo} alt='Kasa logo' className='kasa-logo' />
-                <nav className='nav'>
-                    <NavLink to='/' end="" className='nav-link'>Home</NavLink>
-                    <NavLink to='/about' className='nav-link'>About</NavLink>
+                <img src={logo} alt='Kasa logo' className='header-content__kasa-logo' />
+                <nav className='header-content__nav'>
+                    <NavLink to='/' end="" className='header-content__nav-link'>Home</NavLink>
+                    <NavLink to='/about' className='header-content__nav-link'>About</NavLink>
                 </nav>
             </div>    
         </header>
