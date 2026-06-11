f01ffcba06bd4e458e0c8574d4741883d3a580f2
Igor Vish <igorv0@yahoo.com>
2026-06-10 23:27:31 -0500

    refactor PropertyDetails due to responsive design

--- src/pages/PropertyDetail/index.jsx
+++ src/pages/PropertyDetail/index.jsx
@@ -37,33 +37,33 @@ const PropertyDetail = () => {
                                                                                                 .replace(/^([^ ]* [^ ]*) /, "$1 - ")    // remplacer le deuxième espace par " - "
                                                                                                 .replace(/[ ]/, " , ")                  // remplacer le premier espace par " , "
                                                                               }</h3>
-                    </div>
-                    <div className='property-detail__host'>
-                        <div className='property-detail__host-name'>
-                            <h2 className='property-detail__host-name--first'>{property.host.name.split(' ')[0]}</h2>
-                            <h2 className='property-detail__host-name--last'>{property.host.name.split(' ')[1]}</h2>
+                        <div className='property-detail__description-tags'>
+                            {property.tags.map((tag, index) => (
+                                <button key={`${tag}-${index}`} className='property-detail__description-tags-tag'>
+                                    {tag}
+                                </button>
+                            ))}
                         </div>
-                        <img className='property-detail__host-picture' 
-                            src={property.host.picture}
-                            alt="property host picture" 
-                        />
                     </div>
-                </section>
-                <section className='property-detail__tags-and-rating-container'>
-                    <div className='property-detail__tags'>
-                        {property.tags.map((tag, index) => (
-                            <button key={`${tag}-${index}`} className='property-detail__tags-tag'>
-                                {tag}
-                            </button>
-                        ))}
-                    </div>
-                    <div className='property-detail__rating'>
-                        {Array.from({ length: Number(property.rating) }).map((_, index) => (
-                            <i key={index} className="fa-solid fa-star fa-active" aria-hidden="true"></i>
-                        ))}
-                        {Array.from({ length: 5 - Number(property.rating) }).map((_, index) => (
-                            <i key={index} className="fa-solid fa-star fa-inactive" aria-hidden="true"></i>
-                        ))}
+                    <div className='property-detail__host-and-rating'>
+                        <div className='property-detail__host'>
+                            <div className='property-detail__host-name'>
+                                <h2 className='property-detail__host-name--first'>{property.host.name.split(' ')[0]}</h2>
+                                <h2 className='property-detail__host-name--last'>{property.host.name.split(' ')[1]}</h2>
+                            </div>
+                            <img className='property-detail__host-picture' 
+                                src={property.host.picture}
+                                alt="property host picture" 
+                            />
+                        </div>
+                        <div className='property-detail__rating'>
+                            {Array.from({ length: Number(property.rating) }).map((_, index) => (
+                                <i key={index} className="fa-solid fa-star fa-active" aria-hidden="true"></i>
+                            ))}
+                            {Array.from({ length: 5 - Number(property.rating) }).map((_, index) => (
+                                <i key={index} className="fa-solid fa-star fa-inactive" aria-hidden="true"></i>
+                            ))}
+                        </div>
                     </div>
                 </section>
                 <section className='property-detail__description-and-amenities'>
