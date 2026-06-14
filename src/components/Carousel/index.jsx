d7cbcc6be2d28713acf4d29660670c4b99901463
Igor Vish <igorv0@yahoo.com>
2026-06-14 04:00:01 -0500

    hine arrows and numbers on one-image property

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -3,27 +3,28 @@ import { useState } from 'react'
 
 const Carousel = ({ property }) => {
     const [imagePosition, updateImagePosition] = useState(0);
+
     const propertyImagesSize = property.pictures.length;
-    
+
     const previousImage = () => (imagePosition + propertyImagesSize - 1) % propertyImagesSize;
     const nextImage = () => (imagePosition + 1) % propertyImagesSize;
 
     return (
         <div className='carousel'>
-            <button onClick={() => updateImagePosition(previousImage())}
-                className="carousel__button carousel__button--left" aria-label="Précédent">
+            (<button onClick={() => updateImagePosition(previousImage())}
+                className={propertyImagesSize > 1 ? `carousel__button carousel__button--left` : 'hidden'} aria-label="Précédent">
                     <i className='fa-solid fa-angle-left carousel__button--left-icon'></i>
-            </button >
+            </button >)
             <img
                 src={property.pictures[imagePosition]}
                 alt={property.title}
                 className='carousel__picture'
             />
             <button onClick={() => updateImagePosition(nextImage())} 
-                    className='carousel__button carousel__button--right' aria-label='Suivant'>
+                    className={propertyImagesSize > 1 ? `carousel__button carousel__button--right` : 'hidden'} aria-label='Suivant'>
                         <i className='fa-solid fa-angle-right carousel__button--left-icon'></i>
             </button>
-            <div className='carousel__image-position' >{imagePosition + 1}/{propertyImagesSize}</div>
+            <div className={propertyImagesSize > 1 ? `carousel__image-position` : 'hidden'} > {imagePosition + 1}/{propertyImagesSize}</div>
         </div>
     )
 }
