ca66ac842dacf69e10c1a5352d789acff37459b5
Igor Vish <igorv0@yahoo.com>
2026-06-16 05:48:09 -0500

    404 page responsive design

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,8 +4,10 @@ import logoWhite from '../../assets/logo-white.png'
 const Footer = () => {
     return (
         <footer className='footer-container'>
-            <img src={logoWhite} className='footer-logo' alt='footer logo' />
-            <p>@ 2020 Kasa. All rights reserver</p>
+            <main className='footer-container__content-404'>
+                <img src={logoWhite} className='footer-logo' alt='footer logo' />
+                <p>@ 2020 Kasa. All rights reserver</p>
+            </main>
         </footer>
     )
 }
